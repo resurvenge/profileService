87812411af2b1e7d3ebab7dfc9da5ce564071ba3
resurvenge <159584950+resurvenge@users.noreply.github.com>
2025-01-25 22:08:29 -0500

    Update profileService.lua

--- profileService.lua
+++ profileService.lua
@@ -1,137 +1,141 @@
-local profileService = {}
-local databaseService = game:GetService("DataStoreService")
-local playerService = game:GetService("Players")
-local HttpService = game:GetService("HttpService")
-
-local profileDatabases = {}
-local profiles = {}
-local templates = {}
-local sessionData = {}
-
-local CONFIG = {
-	serviceName = "PROFILESERVICE"
+local Store = {
 }
 
-profileService.__index = profileService
+Store.__index = Store
+
+
+local signals = require(script:WaitForChild("SignalService"))
+
+local dataStoreService = game:GetService("DataStoreService")
+local players = game:GetService("Players")
+local runService = game:GetService("RunService")
+
+Store.Sessions = {}
+
+
+ function Store:storeName(studioStore, liveStore)
+	  if runService:IsStudio() then
+			 return studioStore
+	  else
+			return liveStore
+	  end
+ end
 
 
 
-function profileService:New(dataStoreName)
-	table.insert(profileDatabases, dataStoreName)
-	profileDatabases[dataStoreName] = databaseService:GetDataStore(dataStoreName)
-end
-
-local function getDatabase(dataStoreName)
-	assert(profileDatabases[dataStoreName],"Database is not valid")
-	return profileDatabases[dataStoreName]
-end
-
-function profileService:newTemplate(datatable, dataStoreName)
-	templates[dataStoreName] = datatable
-	for _, obj in pairs(templates[dataStoreName]) do
-		print(obj)
-	end
-end
-
-function profileService:LoadData(player, dataStoreName)
-	local dataStore = getDatabase(dataStoreName)
-	local data = dataStore:GetAsync(player.UserId)
-	if not playerService:IsAncestorOf(player) then
-		player:Kick("[" .. CONFIG.serviceName .. "]: playerService is not an ancestor of " .. player.UserId .. ", " .. player.Name)
-	elseif not data then
-		if templates[dataStoreName] then
-			profiles[player.UserId] = templates[dataStoreName]
-			dataStore:SetAsync(player.UserId, profiles[player.UserId])
-			print("[" .. CONFIG.serviceName .. "]: Created Data")
-		end
-	else
-		print("[" .. CONFIG.serviceName .. "]: Sucessfully loaded data!")
-		profiles[player.UserId] = data
-		print("[" .. CONFIG.serviceName .. "]: TABLE")
-		print(profiles[player.UserId])
-	end
-end
-
-function profileService: Save(player, databaseName)
-	local database = getDatabase(databaseName)
-	assert(profiles[player.UserId], string.format("Profile does not exist %s", player.UserId))
-	for _, obj in pairs(profiles[player.UserId]) do
-		database:SetAsync(player.UserId,profiles[player.UserId])
-		print("[" .. CONFIG.serviceName .. "]: "  .. _ .. obj)
-		print(profiles[player.UserId])
-	end
-end
-
-function profileService: SaveToAttributes(player, databaseName)
-	local database = getDatabase(databaseName)
-	assert(profiles[player.UserId], string.format("Profile does not exist %s", player.UserId))
-	for Name, obj in pairs(profiles[player.UserId]) do
-		if player.Character:GetAttribute(Name) then
-			 profiles[player.UserId][Name] = player.Character:GetAttribute(Name)
-		 	 print("[" .. CONFIG.serviceName  .. "]: " .. Name .. ", " .. player.Character:GetAttribute(Name))
-		end
-		database:SetAsync(player.UserId,profiles[player.UserId])
-		print("[" .. CONFIG.serviceName .. "]: "  .. Name.. obj)
-		print(profiles[player.UserId])
-	end
+function Store.new(Name, template) 
+	  local Instance = setmetatable({}, Store)
+	  Instance.dataStoreName = Name
+	  Instance.dataStore =  dataStoreService:GetDataStore(Instance.dataStoreName)
+	  Instance.profile = {}
+	  Instance.template = template
+	  Instance.data = {}
+	  Instance.Parent = Store
+	  
+	 return Instance
 end
 
 
-function profileService: Update(player, value, callback)
-	assert(profiles[player.UserId][value], string.format("Profile value does not exist %s", player.UserId .. ", value: " .. value))
-	local newData = callback(profiles[player.UserId][value])
-	profiles[player.UserId][value] = newData
-
-	return newData
+function Store:getDataStore() 
+	 assert(self.dataStore, string.format("DataStore does not exist %s", self.dataStoreName))
+	 return self.dataStore
 end
 
-
-function profileService: Get(player, value)
-	assert(profiles[player.UserId][value], string.format("Profile value does not exist %s", player.UserId .. ", value: " .. value))
-	return profiles[player.UserId][value]
-end
-
-function profileService: saveAllPlayers(databaseName)
-	for _, player in pairs(playerService:GetPlayers()) do
-		assert(profiles[player.UserId], string.format("Profile does not exist %s", player.UserId))
-		profileService: Save(player, databaseName)
-	end
-end
-
-function profileService: saveAllPlayersToAttributes(databaseName)
-	for _, player in pairs(playerService:GetPlayers()) do
-		assert(profiles[player.UserId], string.format("Profile does not exist %s", player.UserId))
-		 self:SaveToAttributes(player, databaseName)
-	end
-end
-
-function profileService: autoSave(databaseName, interval, attributes)
-	  return task.spawn(function()
-		while true do
-			  if attributes then
-				self:saveAllPlayersToAttributes(databaseName)
-				print("[" .. CONFIG.serviceName .. "]: ")
-				task.wait(interval)
-			  else
-				self:saveAllPlayers(databaseName)
-				print("[" .. CONFIG.serviceName .. "]: ")
-				task.wait(interval)
+function Store:AssertAsync(player)
+	   return task.spawn(function()
+			  local suc, res = pcall(function()
+					 table.insert(Store.Sessions, player.UserId)
+					 table.insert(self.data, player.UserId)
+					 print(self.data)
+					 self.data[player.UserId] = { status = { saved = false, loaded = true}}
+					 local dataStore = self:getDataStore()
+					 local data = dataStore:GetAsync(player.UserId)
+					 if not players:IsAncestorOf(player) then
+							player:Kick("Player Service error occured, please rejoin.")
+					 end
+					 if not data then
+						   if self.template and not self.profile[player.UserId] then
+								 self.profile[player.UserId] = self.template
+								 dataStore:SetAsync(player.UserId, self.profile[player.UserId])
+								 print("-- PLAYER JOINED FOR THE FIRST TIME: CREATING DATA NOW. ----")
+						   end
+					 else
+						  self.profile[player.UserId] = dataStore:GetAsync(player.UserId)
+					 end
+			  end)
+			  if not suc then
+					 player:Kick("Data error occured..")
 			  end
-		end
+	   end)
+end
+
+function Store: UpdateAsync(player, value, callback)
+	  return task.spawn(function() 
+			 local suc, res = pcall(function() 
+				  assert(self.profile[player.UserId][value], string.format("Profile value does not exist %s", player.UserId .. ", value: " .. value))
+				  print('found')
+				  local newData = callback(self.profile[player.UserId][value])
+				  self.profile[player.UserId][value] = newData
+				  return newData
+			 end)
 	  end)
 end
 
 
-function profileService :__ProfileRemove(player)
-	profiles[player.UserId] = nil
+function Store: GetAsync(player, value)
+	  return task.spawn(function()
+			 local suc, res = pcall(function()
+					assert(self.profile[player.UserId][value], string.format("Profile value does not exist %s", player.UserId .. ", value: " .. value))
+					return self.profile[player.UserId][value] or "Could not alter value."
+			 end)
+	  end)
 end
 
-function profileService: Removing(player)
-	if player.Character then
-		player:Kick("[PROFILESERVICE]: (PLAYER KICK), PLAYER NOT REMOVED")
-	end
-	self:__ProfileRemove(player)
+function Store:SaveAsync(player) 
+	  return task.spawn(function() 
+			  local suc, res = pcall(function() 
+					 if self.profile[player.UserId] and self.dataStore and self.data[player.UserId] then
+							if self.data[player.UserId].status.loaded then
+								self.dataStore:UpdateAsync(player.UserId, function(oldProfileData)
+									if oldProfileData then
+										if not self.data[player.UserId].status.saved then
+											self.data[player.UserId].status.saved = true
+										end
+										return self.profile[player.UserId]
+									else
+										 return {}
+								    end
+								end)
+							else
+								return nil
+							end
+					 end
+			  end)
+			  if not suc then 
+					warn("STORES: [FUNCTION]: SaveAsync, STATUS: THERE WAS A PROBLEM ")
+					player:Kick("Data error occured..")
+			  end
+	  end)
+end
+
+function Store:Release(player) 
+	 return task.spawn(function() 
+			if self.data[player.UserId].status.saved and not Store.Sessions[player.UserId] and self.data[player.UserId].status.loaded then
+				self.profile[player.UserId] = nil
+				self.data[player.UserId] = nil
+				
+				print("profile terminated")
+			elseif not self.data[player.UserId].status.saved then
+				 print(self.data[player.UserId].status.saved)
+				 self:SaveAsync(player)
+				 print(self.data[player.UserId].status.saved)
+			end
+	 end)
 end
 
 
-return profileService
+function Store: getDataStatusTable()
+end
+
+
+return Store
